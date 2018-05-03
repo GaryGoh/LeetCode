9eeaf2d1f30ca13cd96a5410bc41b8038294e667
Mingqian Gao <mgao@guidewire.com>
2018-05-03 17:15:29 +0100

    20180503 - added bestTimetoBuyandSellStock DP

---
+++ src/LeetCode/BestTimetoBuyandSellStockIII.java
@@ -0,0 +1,47 @@
+package LeetCode;
+
+/**
+ * Say you have an array for which the ith element is the price of a given stock on day i.
+ * <p>
+ * Design an algorithm to find the maximum profit. You may complete at most two transactions.
+ * <p>
+ * Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: [3,3,5,0,0,3,1,4]
+ * Output: 6
+ * Explanation: Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+ * Then buy on day 7 (price = 1) and sell on day 8 (price = 4), profit = 4-1 = 3.
+ * Example 2:
+ * <p>
+ * Input: [1,2,3,4,5]
+ * Output: 4
+ * Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+ * Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are
+ * engaging multiple transactions at the same time. You must sell before buying again.
+ * Example 3:
+ * <p>
+ * Input: [7,6,4,3,1]
+ * Output: 0
+ * Explanation: In this case, no transaction is done, i.e. max profit = 0.
+ */
+public class BestTimetoBuyandSellStockIII {
+    public int maxProfit(int[] prices) {
+        int buy1 = Integer.MIN_VALUE, buy2 = Integer.MIN_VALUE;
+        int sell1 = 0, sell2 = 0;
+        for (int p : prices) {
+            buy1 = Math.max(buy1, -p);
+            sell1 = Math.max(sell1, p + buy1);
+            buy2 = Math.max(buy2, sell1 - p);
+            sell2 = Math.max(sell2, buy2 + p);
+        }
+        return sell2;
+    }
+
+    public static void main(String[] args) {
+        BestTimetoBuyandSellStockIII b = new BestTimetoBuyandSellStockIII();
+        int[] p = {7,6,4,3,1};
+        System.out.println(b.maxProfit(p));
+    }
+}
